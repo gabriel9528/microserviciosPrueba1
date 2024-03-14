e2dd9fe663944a9da4bb606e2d16165a0dd658ef
Gabriel <gabrielferv95@gmail.com>
2024-03-14 10:39:36 -0500

    hystrix

--- shoppingService/src/main/java/com/microservicios1/shoppingService/Client/CustomerClient.java
+++ shoppingService/src/main/java/com/microservicios1/shoppingService/Client/CustomerClient.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/customers")
 @FeignClient(name = "customerService")
 public interface CustomerClient {
     @GetMapping("/{id}")
--- shoppingService/src/main/java/com/microservicios1/shoppingService/Client/ProductClient.java
+++ shoppingService/src/main/java/com/microservicios1/shoppingService/Client/ProductClient.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping(value = "/products")
 @FeignClient(name = "productService")
 public interface ProductClient {
     @GetMapping("/{id}")
